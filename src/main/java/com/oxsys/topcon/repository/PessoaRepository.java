7a532628b65b690b9ab2c60667c04a4d58683134
lyndon tavares <integraldominio@gmail.com>
2015-12-16 17:31:40 -0200

    news

--- src/main/java/com/oxsys/topcon/repository/PessoaRepository.java
+++ src/main/java/com/oxsys/topcon/repository/PessoaRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.oxsys.topcon.model.Pessoa;
+import com.oxsys.topcon.model.enums.PessoaSituacao;
+
 
 @RepositoryRestResource(collectionResourceRel = "pessoa", path = "pessoa", exported = true)
 public interface PessoaRepository extends PagingAndSortingRepository<Pessoa, Long>  {
 	
-	Iterable<Pessoa> findByIdOrNomeOrSituacao( long id, String nome, String situacao );
+	Iterable<Pessoa> findByIdOrNomeOrSituacao( long id, String nome, PessoaSituacao  situacao );
 }
--- src/main/java/com/oxsys/topcon/resource/PessoaResource.java
+++ src/main/java/com/oxsys/topcon/resource/PessoaResource.java
@@ -1,23 +1,32 @@
 package com.oxsys.topcon.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oxsys.topcon.model.Pessoa;
+import com.oxsys.topcon.model.enums.PessoaSituacao;
 import com.oxsys.topcon.repository.PessoaRepository;
 
 @RestController
 @RequestMapping("/api")
 public class PessoaResource {
 
-	
+@Autowired	
 private PessoaRepository repo;
 	
 	@RequestMapping( value="/pessoa/filtro", method = RequestMethod.POST )
-	public Iterable<Pessoa> listaPessoas(@RequestBody final  Pessoa pessoa){
-		return repo.findAll();
+	public Iterable<Pessoa> listaPessoas(@RequestBody  Pessoa pessoa){
+		
+		System.out.println(pessoa);
+		if(pessoa.getSituacao() == null){
+			pessoa.setSituacao( PessoaSituacao.ATIVA  );
+		}
+		
+		return repo.findByIdOrNomeOrSituacao( pessoa.getId() , pessoa.getNome() , pessoa.getSituacao().toString()  );
+		
 	}
 	
 	
